892dfbd12d3934b219c425cf1dd401e25a6328eb
Adriaan Knapen <a.d.knapen@protonmail.com>
2017-11-30 14:47:03 +0100

    dev

--- ex3/1.py
+++ ex3/1.py
@@ -1,6 +1,12 @@
 from crypto.cyphers.RC4 import RC4
 from crypto.key.generate import generate_key
 from collections import Counter
+import matplotlib.pyplot as plt
+'''
+Requires matplotlib, install with
+python -mpip install -U pip
+python -mpip install -U matplotlib
+'''
 
 result = []
 for _ in range(0, 100000):
@@ -12,3 +18,15 @@ for _ in range(0, 100000):
 
 c = Counter(result)
 print(c.most_common())
+
+num_bins = 256
+fig, ax = plt.subplots()
+n, bins, patches = ax.hist(result, num_bins, normed=1)
+
+# Set the axis labels and the title of the plot
+ax.set_xlabel('Output value of second generated value')
+ax.set_ylabel('Probability density')
+ax.set_title('Distribution of second output generated with RC4 with random keys')
+
+# Show the plot
+plt.show()
