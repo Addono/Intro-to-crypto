0c2621cca0c7d056d8430db21317b0157c3d1d1a
Adriaan Knapen <a.d.knapen@protonmail.com>
2017-12-04 21:14:20 +0100

    dev

--- hw1/ex_3.py
+++ hw1/ex_3.py
@@ -1,7 +1,9 @@
+from multiprocessing import Process, Queue, Lock, freeze_support
 import time
 
-from crypto.cyphers.RC4 import RC4
+from tqdm import tqdm
 
+from crypto.cyphers.RC4 import RC4
 
 def increment_key(key: list, l: int = 0, max: int = 256) -> list:
     i = len(key)
@@ -26,31 +28,72 @@ def check_key(key: list, expected: list) -> bool:
 
 
 def crack_key(start_key: list, expected_output: list, fixed_digits: int = 0) -> list:
-    print(start_key)
     while start_key is not None and not check_key(start_key, expected_output):
         start_key = increment_key(start_key, fixed_digits)
 
     return start_key
 
-total_key_length = 5
-split = 3
-given_key_start = [80]
-expected_output = [130, 189, 254, 192, 238, 132, 216, 132, 82, 173]
 
-tm = time.time()
+def worker(expected_output, fixed_digits):
 
-given_key_length = len(given_key_start)
-
-start_base_key = given_key_start + [0 for _ in range(split - given_key_length)]
-attack_length = total_key_length - split
-
-while start_base_key is not None:
-    attack_key = start_base_key + [0 for _ in range(attack_length)] # Generate the key to attack
-    crack_key(attack_key, expected_output, split)
-
-    start_base_key = increment_key(start_base_key, given_key_length)
-
-# print(crack_key(start_key, expected_output, 3))
+    while True:
+        start_key = q.get()
+        if start_key is None:
+            break
+        result = crack_key(start_key, expected_output, fixed_digits)
+        if result is not None:
+            print(result)
+        q.task_done()
+        pbar.update(1)
 
 
-print(time.time() - tm)
+if __name__ == '__main__':
+    total_key_length = 5
+    split = 3
+    given_key_start = [80]
+    expected_output = [130, 189, 254, 192, 238, 132, 216, 132, 82, 173]
+    max_processes = 5
+
+    tm = time.time()
+
+    given_key_length = len(given_key_start)
+
+    start_extension_length = split - given_key_length
+    start_base_key = given_key_start + [0 for _ in range(start_extension_length)]
+    attack_length = total_key_length - split
+    start_base_key_extension = [0 for _ in range(attack_length)]
+
+    # Fill the queue
+    q = Queue()
+    with tqdm(total=256 ** start_extension_length, unit="sub-keys", desc="Adding sub-keys to queue") as pbar:
+        while start_base_key is not None:
+            attack_key = start_base_key + start_base_key_extension  # Generate the key to attack
+
+            q.put(attack_key)
+            pbar.update(1)
+
+            start_base_key = increment_key(start_base_key, given_key_length)
+
+    # Spawn all processes
+    lock = Lock()
+    processes = []
+    lock.acquire()
+    for _ in tqdm(range(max_processes), desc="Spawning processes"):
+        process = q.Process(target=worker, args=(expected_output, split))
+        process.start()
+        process.join()
+        processes.append(process)
+
+    pbar = tqdm(total=256 ** start_extension_length)
+
+    # Wait for the queue to be emptied
+    q.get()
+    pbar.close()
+
+    # Kill all workers
+    for _ in tqdm(range(max_processes), desc="Sending kill signal to all workers"):
+        q.put(None)
+    for t in tqdm(processes, desc="Wait will all workers are finished"):
+        t.join()
+
+    print(time.time() - tm)
